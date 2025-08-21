001bf28ee4ed023409ae30c61f1347b37589992c
Akash Kumar Shah <208366876+akashshah10@users.noreply.github.com>
2025-08-21 20:02:12 +0530

    Add files via upload

---
+++ 03_Loops/Sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+    cout << "Enter the number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    cout << "The sum is: " << sum;
+    return 0;
+}
