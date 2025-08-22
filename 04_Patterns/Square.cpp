fd5c7b0e8d2b608b1014b32b49b0525a72c93139
Akash Kumar Shah <208366876+akashshah10@users.noreply.github.com>
2025-08-22 11:53:23 +0530

    Add files via upload

---
+++ 04_Patterns/Square.cpp
@@ -0,0 +1,20 @@
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
