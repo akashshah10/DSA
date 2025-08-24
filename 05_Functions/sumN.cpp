fcf0a145ebddd83cc6a8c7f43cd5508cf6ea3911
Akash Kumar Shah <208366876+akashshah10@users.noreply.github.com>
2025-08-24 12:14:51 +0530

    Add files via upload

---
+++ 05_Functions/sumN.cpp
@@ -0,0 +1,19 @@
+// Calculate sum of n natural numbers
+#include <iostream>
+using namespace std;
+
+int sumN(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int main()
+{
+    cout << sumN(10);
+    return 0;
+}
