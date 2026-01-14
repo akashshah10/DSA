a987f2f74059af2e2f3cd860b3dbebd151614362
akashshah10 <officialakashshah10@gmail.com>
2026-01-14 13:01:48 +0530

    Optimal

--- Leetcode/11.cpp
+++ Leetcode/11.cpp
@@ -5,19 +5,40 @@
 
 using namespace std;
 
+
 int mostWater(vector<int>& height)
 {
-    int container, maxWater = 0;
-    for(int i = 0; i < height.size()-1; i++)
+    //--> Optimal Approach(USing 2 pointer)
+    int maxWater = 0, lp = 0, rp = height.size() - 1;
+    while(lp < rp)
     {
-        for(int j = i; j < height.size(); j++)
-        {
-            container = min(height[i],height[j]) * (j-i);
-        }
-        maxWater = max(container, maxWater);
+        int width = rp - lp;
+        int ht = min(height[lp], height[rp]);
+        int currWater = width * ht;
+        maxWater = max(currWater,maxWater);
+
+        height[lp] < height[rp] ? lp++ : rp--;
     }
     return maxWater;
+    
+    
 }
+
+//--> Brute Force Approach
+//{ 
+//     int currWater, maxWater = 0;
+//     for(int i = 0; i < height.size()-1; i++)
+//     {
+//         for(int j = i+1; j < height.size(); j++)
+//         {
+//             currWater = min(height[i],height[j]) * (j-i);
+//         }
+//         maxWater = max(currWater, maxWater);
+//     }
+//     return maxWater;
+// }
+
+
 int main()
 {
     vector<int> height = {1,8,6,2,5,4,8,3,7};
