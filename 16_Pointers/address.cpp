f0e486c9edd170ddf474258b7a6441be6a515b67
Akash Kumar Shah <208366876+akashshah10@users.noreply.github.com>
2026-03-07 10:11:24 +0530

    Add files via upload

---
+++ 16_Pointers/address.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 8;
+    int *ptr = &a;
+    int **ptr2 = &ptr;
+    cout << ptr2 << endl;
+    cout << ptr << endl;
+    cout << &a << endl;
+    return 0;
+}
