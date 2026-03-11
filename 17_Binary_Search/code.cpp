fde6a66f2778e2edd1ca5624553158acae0ee871
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-03-11 07:56:30 +0530

    Updated Binary Search

--- 17_Binary_Search/code.cpp
+++ 17_Binary_Search/code.cpp
@@ -1,8 +1,35 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
+
+int binarySearch(vector<int> arr, int tar)
+{
+    int st = 0, end = arr.size() - 1;
+    while (st <= end)
+    {
+        int mid = (st + end) / 2;
+        if (tar > arr[mid])
+        {
+            st = mid + 1;
+        }
+        else if (tar < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else if (tar == arr[mid])
+        {
+            return mid;
+        }
+    }
+
+    return -1;
+}
 
 int main()
 {
-    int arr[] = {-1, 0, 3, 4, 5, 9, 2};
+    vector<int> arr1 = {-1, 0, 3, 4, 5, 9, 12};
+    int tar = 12;
+    cout << binarySearch(arr1, tar) << endl;
+
     return 0;
 }
